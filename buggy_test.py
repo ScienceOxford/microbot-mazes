a03c159b07672a9b368f7d0ea6bb7483a1c4ac0f
Sarah Townson <sarah.townson@scienceoxford.com>
2019-03-05 10:56:42 +0000

    Create buggy_test.py

---
+++ buggy_test.py
@@ -0,0 +1,81 @@
+from microbit import *
+
+# Please tag us if used!
+# We'd love to see what you make:
+# @ScienceOxford
+
+'''
+This tells us which of the micro:bit's pins is connected to which input on the motor driver (follow the coloured wires!).
+e.g. FL means that it controls the pin that turns on the left-hand motor in the forward direction.
+'''
+FL = pin14
+BL = pin13
+FR = pin12
+BR = pin15
+
+'''
+If the pin is set to HIGH (1023), the motor is turned off. The lower the number, the faster the motor goes.
+Currently the motors are set to turn on at half speed (511), as this makes it easier to control.
+'''
+on = 511
+off = 1023
+
+'''
+The following functions define the combination of pins to control direction.
+'''
+def stop(time=100):
+    display.clear()
+    FL.write_analog(off)
+    BL.write_analog(off)
+    FR.write_analog(off)
+    BR.write_analog(off)
+    sleep(time)
+
+def forward(time, speed=on):
+    display.show(Image.ARROW_N)
+    FL.write_analog(speed)
+    BL.write_analog(off)
+    FR.write_analog(speed)
+    BR.write_analog(off)
+    sleep(time)
+    stop()
+
+def backward(time, speed=on):
+    display.show(Image.ARROW_S)
+    FL.write_analog(off)
+    BL.write_analog(speed)
+    FR.write_analog(off)
+    BR.write_analog(speed)
+    sleep(time)
+    stop()
+
+def left_turn(time, speed=on):
+    display.show(Image.ARROW_W)
+    FL.write_analog(off)
+    BL.write_analog(speed)
+    FR.write_analog(speed)
+    BR.write_analog(off)
+    sleep(time)
+    stop()
+
+def right_turn(time, speed=on):
+    display.show(Image.ARROW_E)
+    FL.write_analog(speed)
+    BL.write_analog(off)
+    FR.write_analog(off)
+    BR.write_analog(speed)
+    sleep(time)
+    stop()
+
+stop()
+
+# WRITE YOUR CODE ON THE NEXT LINE BELOW HERE
+
+while True:
+    if button_a.was_pressed():
+        forward(1000)             # test each direction
+        backward(1000)
+        left_turn(1000, 200)
+        right_turn(1000, 200)
+    if button_b.was_pressed():
+        forward(5000, 0)          # test quick forward, to check how straight it goes
